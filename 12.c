e0611874c27d289f15180e4619a9a4b1a4866431
RWXogenisis <65536776+RWXogenisis@users.noreply.github.com>
2023-10-14 00:39:16 +0530

    Update 12.c hacktoberfest 2023
    Distance Vector Hacktoberfest 2023

--- 12.c
+++ 12.c
@@ -1,7 +1,44 @@
-#include<stdio.h>
-int main()
-{
-    printf("hello");
+//Distance vector client
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define PORT 8888
+
+typedef struct DistanceVector {
+    int node;
+    int cost;
+} DistanceVector;
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+
+    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_socket < 0) {
+        perror("Error in socket creation");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = PORT;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    DistanceVector dv;
+    dv.node = 1;
+    dv.cost = 0;
+
+    while (1) {
+        printf("Sending distance vector to server...\n");
+        sendto(client_socket, &dv, sizeof(dv), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        sleep(5); // Send the distance vector periodically
+    }
+
+    close(client_socket);
     return 0;
 }
-//Sample type your code.
